a4deba395b3436aec7deec35d477cfd86c1a3357
nilesh7272 <nileshparadva97@gmail.com>
2024-07-08 19:03:55 +0530

    file upload

---
+++ js/slick-slider.js
@@ -0,0 +1,28 @@
+$('.center').slick({
+    centerMode: true,
+    dots: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                dots: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
