3d769364871e02643ea9c444188606257b6a867b
nilesh7272 <nileshparadva97@gmail.com>
2024-07-09 18:20:09 +0530

    file upload

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -26,3 +26,32 @@ $('.center').slick({
         }
     ]
 });
+
+$('.center-1').slick({
+    centerMode: true,
+    dots: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                dots: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
