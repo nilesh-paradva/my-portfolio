98149db95b3c9bae45d1de749e27e71c5723ed35
nilesh7272 <nileshparadva97@gmail.com>
2024-07-28 17:58:46 +0530

    add logo & confeit button

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,24 @@
-$(window).on('load',function(){
+const celebrateBtn = document.getElementById('celebrateBtn');
+
+celebrateBtn.addEventListener('click', () => {
+    // trigger confetti
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+
+    // button animation
+    celebrateBtn.style.transform = 'scale(0.95)';
+    setTimeout(() => {
+        celebrateBtn.style.transform = 'scale(1)';
+    }, 100);
+});
+
+$(window).on('load', function () {
     $('.loader-wrepper').delay('1200').fadeOut('fast')
 });
+
 
 $(window).scroll(function () {
     if ($(window).scrollTop() > 200) {
